4aed623d7d0e0b4d4ad66eb9671600d7151458ab
chandrakatore2004 <chandrakatore@gmail.com>
2025-11-19 23:18:40 +0530

    Modify backend configuration in backend.tf
    Updated S3 backend configuration and enabled locking.

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,20 +1,21 @@
 terraform {
-  required_version = "~> 1.12.1"
+  required_version = "~> 1.12.1"   # ensure this matches your environment
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
   }
+
   backend "s3" {
-    bucket         = "chandra-terraform-eks-state-s3-bucket"
-    region         = "ap-south-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+    bucket       = "chandra-terraform-eks-state-bucket"
+    region       = "ap-south-1"
+    key          = "eks/terraform.tfstate"
+    use_lockfile = true     # <--- enable native S3 locking
+    encrypt      = true
   }
 }
 
 provider "aws" {
-  region  = var.aws-region
+  region = var.aws_region
 }
